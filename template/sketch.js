df4a28f0d7f64b7c420bca34fcc1a11682b067fb
mohit <mohitudupa@gmail.com>
2020-04-13 09:46:35 +0530

    Added p5 template code

---
+++ template/sketch.js
@@ -0,0 +1,12 @@
+function setup(){
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
+    canvas.parent("canvas");
+
+    frameRate(60)
+    background(33,33,33);
+}
+
+
+function draw(){
+
+}
